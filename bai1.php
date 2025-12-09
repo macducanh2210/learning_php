92c7912fa09d3bbe1e84aa99e779e4d9bc8c057b
mac duc anh <pk90x123@gmail.com>
2025-12-09 16:24:39 +0700

    hoan thien vi du tren lop

--- bai1.php
+++ bai1.php
@@ -33,10 +33,52 @@
         showvalue();
         echo $a;
     ?>
+    <?php 
+    $a=1;
+    $b=2;
+    function Sum(){
+        global $a, $b;
+        $b=$a+$b;
+    }
+    Sum();
+    echo $b;//in ra 3
+    ?>
+     <?php
+        $name = "Nguyễn Minh Đức";
+        $year = 2000;
+        $city = "Hà Nội";
+
+        echo "<p>Giá trị của biến name là chuỗi: $name</p>";
+        echo "<p>Giá trị của biến year là số: $year</p>";
+        echo "<p>Giá trị của biến city là chuỗi: $city</p>";
+    ?>
+
     <?php
-        /*khai bao hang so pi co gia tri la 3.14*/
-        define('pi',3.14);
-        echo pi;
+        define("PI", 3.14);
+        echo "Giá trị của hằng số PI là: " . PI;
+    ?>
+
+    <?php
+        echo "<h1> Tiêu đề lớn</h1>";
+        echo "<p style ='color:red' >Dòng chữ này có màu gì</p>";
+    ?>
+
+    <?php
+        for ($i = 1; $i <= 10; $i++) {
+            if ($i == 5) {
+                break; 
+            }
+            echo " $i<br>";
+        }
+    ?>
+
+    <?php
+        for ($i = 1; $i <= 5; $i++) {
+            if ($i == 3) {
+                continue; // bỏ qua vòng lặp hiện tại và sang vòng tiếp theo
+            }
+            echo "Giá trị của i bo qua 3 la: $i<br>";
+        }
     ?>
 </body>
 </html>
